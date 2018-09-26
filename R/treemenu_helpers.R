b002e03d9096b2b836a00a6a7bcd3fbe6e56d5b4
emillykkejensen <emil@emillykkejensen.dk>
2018-09-26 13:57:21 +0000

    Minor bug fix in HTML

--- R/treemenu_helpers.R
+++ R/treemenu_helpers.R
@@ -22,10 +22,10 @@ treemenu_linkBuilder <- function(val, level, row, label, icon){
 treemenu_list <- function(id, level, row, label, icon, openid){
 
   paste0("<li class=\"level", level, "\">
-            <label for=\"", id, "\"></label>
-            <a href=\"", treemenu_setInputTreeValue(id, level, row), "\">
-              <i class=\"fa fa-", icon, "\" aria-hidden=\"true\"></i> ", label, "
-            </a>
+            <label for=\"", id, "\"></label>",
+            "<a href=\"", treemenu_setInputTreeValue(id, level, row), "\">",
+              "<i class=\"fa fa-", icon, "\" aria-hidden=\"true\"></i> ", label,
+            "</a>
             <input type=\"checkbox\" id=\"", id, "\" onclick=\"javascript:Shiny.setInputValue('treemenu_open', {openid:'", openid, "', randomVal:Math.random()});\"/>
             <ol id=\"treemenu_open_", openid, "\">
               <li class=\"loading\">
